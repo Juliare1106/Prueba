448e0020649bd7358e8e921778c6c9f473423ea5
Juliare1106 <56942969+Juliare1106@users.noreply.github.com>
2019-10-31 10:52:05 -0500

    Inicial
    Primera versión modificada del archivo

--- prueba.php
+++ prueba.php
@@ -1,8 +1,12 @@
 <html>
  <head>
-  <title>Prueba git hub</title>
+  <title>Versión 2 Git HUB</title>
  </head>
  <body>
  <?php echo '<p>Hola Mundo, este es el archivo inicial que se cargará al repositorio</p>'; ?>
+  <?php echo '<p>En este archivo aplicamos los cambios para hacer nuevamente un comit y subir el archivo con los cambios reflejados</p>'; ?>
+  <?php echo '<p>Presentado por:</p>'; ?>
+  <?php echo '<p>Julián Andrés Arévalo Hernandez </p>'; ?>
+  <?php echo '<p>ID 000386352 </p>'; ?>
  </body>
 </html>
